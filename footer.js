ae5b9fdbd21889e3323670bf589b84cc0056e307
ArchismanDutta <165658143+ArchismanDutta@users.noreply.github.com>
2024-10-05 13:04:58 +0530

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./footer.css";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-content">
+        Made by Archisman Dutta ❤️
+        
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
